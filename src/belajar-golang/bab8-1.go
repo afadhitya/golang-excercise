9cd6c4a786b2589ab23953a6d324cd1c996120ad
Achmad Fadhitya <afadhitya@gmail.com>
2019-11-10 22:35:10 +0700

    init commit

---
+++ src/belajar-golang/bab8-1.go
@@ -0,0 +1,34 @@
+package main
+
+// import "fmt"
+
+// func main(){
+// 	var firstName string = "fadhit"
+
+// 	var lastName string = "muharram"
+
+// 	iniTanpaTipeData := "dadang"
+
+// 	lastName = "adit aja deh"
+
+// 	var satu, dua ,tiga = "satu", "dua", "tiga"
+
+// 	var iniInt, iniBool, iniStr, iniFloat = 1, true, "ini string", 5.6
+
+// 	fmt.Printf("hallo %s %s!\n", firstName, lastName)
+// 	fmt.Println("haloo", firstName, lastName + "!")
+// 	fmt.Println(iniTanpaTipeData)
+
+// 	fmt.Println(satu, dua, tiga)
+
+// 	fmt.Println(iniInt, iniBool, iniStr, iniFloat)
+
+// 	newInteger := new(float32)
+
+// 	if (iniBool){
+// 		fmt.Println("betul")
+// 	}
+
+// 	fmt.Println(newInteger)
+
+// }
---
+++ src/belajar-golang/bab9-1.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	var positifBesar int = 9223372036854775807
+
+	fmt.Println(positifBesar)
+}
