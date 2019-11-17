05b7d593b72305dfc85bb5b21f33fbe7076a37e2
Achmad Fadhitya <afadhitya@gmail.com>
2019-11-17 15:16:06 +0700

    new exercise

--- src/belajar-golang/bab9-1.go
+++ src/belajar-golang/bab9-1.go
@@ -1,9 +1,9 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-func main(){
-	var positifBesar int = 9223372036854775807
+// func main(){
+// 	var positifBesar int = 9223372036854775807
 
-	fmt.Println(positifBesar)
-}
+// 	fmt.Println(positifBesar)
+// }
